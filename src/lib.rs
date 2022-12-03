d40bb6a70dc3b01eece7bb9074320e20b67284a4
Rupert Foggo McKay <maxfmckay@gmail.com>
2022-12-03 15:27:34 +0100

    Solves day02 part 1 and 2

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,18 @@
-pub mod day01;
-pub mod day02;
-pub mod utils;
+use std::fs;
+pub mod day01caloriecounting;
+pub mod day02rockpaperscissors;
+pub mod utils {
+    use super::*;
+    pub fn get_file_contents(file_path: &str) -> String {
+        fs::read_to_string(file_path)
+            .unwrap()
+            .replace("\r\n", "\n")
+            .replace("\r", "\n")
+    }
+    pub fn get_lines(file_path: &str) -> Vec<String> {
+        get_file_contents(file_path)
+            .lines()
+            .map(ToOwned::to_owned)
+            .collect()
+    }
+}
